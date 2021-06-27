1092914b35498cb0d17ce16de471a25890422ff7
Barry Sharpen <bsharpen@sol-tec.com>
2021-06-27 21:47:49 +0100

    amended nsg rule

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ resource "azurerm_network_security_group" "nsg" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "3389"
-    source_address_prefix      = "*"
+    source_address_prefix      = "10.0.0.1/24"
     destination_address_prefix = "*"
   }
 }
